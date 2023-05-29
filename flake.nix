bb3fa6e15c6d753cb25481b2022d62e5f6ab9866
Richard Smith <appsmithuk@outlook.com>
2023-05-29 23:59:50 +0100

    Add mount-zip package

--- flake.nix
+++ flake.nix
@@ -1,21 +1,12 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "nixpkgs";
   outputs = { self, nixpkgs }:
     let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system:
+        import ./default.nix { pkgs = import nixpkgs { inherit system; }; });
     };
 }
